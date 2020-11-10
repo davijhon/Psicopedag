15023ca20a16ef56346284ddd5c2e770ba9239ba
Dave <davidalejandrodand@gmail.com>
2020-11-10 12:25:40 -0400

    add variable for deploy heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn psicopedag.wsgi
--- settings/prod.py
+++ settings/prod.py
@@ -8,9 +8,22 @@ ALLOWED_HOSTS = []
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'template1',
+#         'USER': 'postgres',
+#         'PASSWORD': 'DaveAle26$',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+STATICFILES_DIRS = (BASE_DIR, 'static/')
