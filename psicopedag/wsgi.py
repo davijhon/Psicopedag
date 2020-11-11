eae46c827bf1357fac4cb56d8c70c63ac25278ef
Dave <davidalejandrodand@gmail.com>
2020-11-11 17:59:50 -0400

    switch to prod

--- psicopedag/wsgi.py
+++ psicopedag/wsgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.local')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings.prod')
 
 from dj_static import Cling # para static file
 
