6902655a313b6426b57c57212c47466f6ab10c55
Dave <davidalejandrodand@gmail.com>
2020-11-12 17:23:32 -0400

    fix some static errors

--- psicopedag/urls.py
+++ psicopedag/urls.py
@@ -7,6 +7,7 @@ from django.urls import path, re_path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls', namespace='pages')),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG == False:
