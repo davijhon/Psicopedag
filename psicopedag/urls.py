701d1475c369763be6edbbe5923e92de598d8fa5
Dave <davidalejandrodand@gmail.com>
2020-11-12 16:56:24 -0400

    switching media root

--- psicopedag/urls.py
+++ psicopedag/urls.py
@@ -7,16 +7,13 @@ from django.urls import path, re_path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls', namespace='pages')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG == False:
     handler400 = 'pages.views.error_400'
     handler403 = 'pages.views.error_403'
     handler404 = 'pages.views.error_404'
     handler500 = 'pages.views.error_500'
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if "debug_toolbar" in settings.INSTALLED_APPS:
     import debug_toolbar
