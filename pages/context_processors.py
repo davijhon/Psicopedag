d0cbf581752b0940088ee9315242846963a04e0b
Dave <davidalejandrodand@gmail.com>
2020-11-12 13:04:40 -0400

    fix bug

--- pages/context_processors.py
+++ pages/context_processors.py
@@ -1,8 +1,7 @@
-from django.conf import settings
 from decouple import config
 
 def get_tracking_code_google_analytics(request):
-    GOOGLE_ANALYTICS_ID = settings.GOOGLE_ANALYTICS_ID
+    GOOGLE_ANALYTICS_ID = config('GOOGLE_ANALYTICS_ID')
     return {
         'GOOGLE_ANALYTICS_ID': GOOGLE_ANALYTICS_ID,
     }
