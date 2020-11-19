55b48fe16ebf71dbc43b94a1815240aae08e89be
Dave <davidalejandrodand@gmail.com>
2020-11-19 15:21:02 -0400

    now what?

--- settings/prod.py
+++ settings/prod.py
@@ -5,7 +5,7 @@ from decouple import config
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['psicopedagogia-integral.herokuapp.com']
 
