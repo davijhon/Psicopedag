7ae330c29250f8c3cd4b3824890e38ebd0711e41
Dave <davidalejandrodand@gmail.com>
2020-11-20 14:34:40 -0400

    whitenoise was commented

--- settings/base.py
+++ settings/base.py
@@ -120,7 +120,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATICFILES_FINDERS = [
 
